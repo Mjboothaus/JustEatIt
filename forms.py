96bc0bbee90b98ee539192bca22391e3e5f81745
mjboothaus <mjboothaus@gmail.com>
2017-11-03 12:11:27 -0700

    Progress commit - progress commit - finished insert of Bokeh placeholder

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SelectField
 from wtforms.validators import DataRequired, EqualTo, Length
 
+
 # Set your classes here.
 
 
@@ -24,7 +25,6 @@ class RegisterForm(FlaskForm):
         'Age range', validators=[DataRequired(), Length(min=1, max=10)]
     )
 
-
     # Age range 0-2, 3-6, 7-10, 11-15, 16-21, 22-30, 31-40, 41-50, 51-70, 70+
 
     # Sex: F / M / Other
@@ -35,7 +35,7 @@ class RegisterForm(FlaskForm):
     confirm = PasswordField(
         'Repeat Password',
         [DataRequired(),
-        EqualTo('password', message='Passwords must match')]
+         EqualTo('password', message='Passwords must match')]
     )
 
 
@@ -49,16 +49,20 @@ class ForgotForm(FlaskForm):
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
 
+
 class SpecifyProfileForm(FlaskForm):
     dietary_types = [('Allergy', 'Allergy'), ('Belief', 'Belief'), ('Health', 'Health'),
                      ('Preference', 'Preference'), ('Wellness', 'Wellness'),
                      ('Other  specify', 'Other  specify')]
 
-    allergy_profiles = [['milk'], ['egg'], ['peanut'], ['milk', 'egg']]        # TODO: Populate dynamically?
+    allergy_profiles = [['milk'], ['egg'], ['peanut'], ['milk', 'egg']]  # TODO: Populate dynamically?
 
     profile_list = []
     for i, profile in enumerate(allergy_profiles):
-        profile_list.append((i+1, profile))
+        if len(profile) == 1:
+            profile_list.append(((i + 1), profile[0]))
+        else:
+            profile_list.append((i + 1, ', '.join(profile)))
 
     profile = SelectField(label='profile_choice', description="Please choose an allergy profile", default=1,
-                      choices=profile_list)
+                          choices=profile_list)
