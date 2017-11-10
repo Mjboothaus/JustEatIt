4f0f44c3c72a558488524f27646268eebc5cde54
mjboothaus <mjboothaus@gmail.com>
2017-11-10 13:34:58 +1100

    Progress commit - moved DEBUG flag into app.run() so that it should only be active when running locally.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+# DEBUG = True  -- turned off - now in the app.run() call if running locally
 
 # Secret key for session management. You can generate random strings here:
 # http://clsc.net/tools-old/random-string-generator.php
@@ -12,12 +12,13 @@ SECRET_KEY = 'my precious dona'
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
-# Yummly
+# Yummly mjboothaus Account: Hackathon Plan - Access granted 24 July 2017
+
+API_ID     = 'b4f167ed'
 SECRET_KEY = '7d441f27d441f27567d441f2b6176a'
 
-# default option values
+# Default option values (for Yummly connections)
+
 TIMEOUT = 15.0
 RETRIES = 2
 
-# Yummly mjboothaus Account: Hackathon Plan - Access granted 24 July 2017
-API_ID = 'b4f167ed'
