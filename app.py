9bdc573b4f715aa40a787edb078af4467221b98c
mjboothaus <mjboothaus@gmail.com>
2017-11-10 12:27:06 +1100

    Progress commit - country data being read in and passing of dynamic allergy index to map working.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import yummly_wrapper
 
 import pandas as pd
 import numpy as np
+from random import random, randint, seed
 
 ## import holoviews as hv      ## NOTE: For the future -- HoloViews is a nice wrapper for Bokeh
 ## hv.extension('bokeh')
@@ -132,35 +133,37 @@ def top10():
 def eat_map():
     # Grabs the folder where the script runs
     basedir = os.path.abspath(os.path.dirname(__file__))
-
     APP_STATIC = os.path.join(basedir, 'static')
+    csv_filepath = APP_STATIC + '/data/country_info.csv'
 
-    path = APP_STATIC + '/data/country_info.csv'
+    seed(42)      # Set a constant starting seed
 
-    country_codes = pd.read_csv(path)
+    country_codes = pd.read_csv(csv_filepath, header=0, names={'Country', 'Code'})
 
-    country_dict = country_codes.to_dict()
+    country_codes['AllergyIndex'] = country_codes['Code'].apply(lambda x: randint(0, len(country_codes)))
 
-    country_data = [
-        ["BLR", 75], ["BLZ", 43], ["RUS", 50], ["RWA", 88], ["SRB", 21], ["TLS", 43],
-        ["REU", 21], ["TKM", 19], ["TJK", 60], ["ROU", 4], ["TKL", 44], ["GNB", 38],
-        ["GUM", 67], ["GTM", 2], ["SGS", 95], ["GRC", 60], ["GNQ", 57], ["GLP", 53],
-        ["JPN", 59], ["GUY", 24], ["GGY", 4], ["GUF", 21], ["GEO", 42], ["GRD", 65],
-        ["GBR", 14], ["GAB", 47], ["SLV", 15], ["GIN", 19], ["GMB", 63], ["GRL", 56],
-        ["ERI", 57], ["MNE", 93], ["MDA", 39], ["MDG", 71], ["MAF", 16], ["MAR", 8],
-        ["MCO", 25], ["UZB", 81], ["MMR", 21], ["MLI", 95], ["MAC", 33], ["MNG", 93],
-        ["MHL", 15], ["MKD", 52], ["MUS", 19], ["MLT", 69], ["MWI", 37], ["MDV", 44],
-        ["MTQ", 13], ["MNP", 21], ["MSR", 89], ["MRT", 20], ["IMN", 72], ["UGA", 59],
-        ["TZA", 62], ["MYS", 75], ["MEX", 80], ["ISR", 77], ["FRA", 54], ["IOT", 56],
-        ["SHN", 91], ["FIN", 51], ["FJI", 22], ["FLK", 4], ["FSM", 69], ["FRO", 70],
-        ["NIC", 66], ["NLD", 53], ["NOR", 7], ["NAM", 63], ["VUT", 15], ["NCL", 66],
-        ["NER", 34], ["NFK", 33], ["NGA", 45], ["AUS", 96], ["NPL", 21], ["NRU", 13],
-        ["NIU", 6], ["COK", 19], ["XKX", 32], ["CIV", 27], ["CHE", 65], ["COL", 64],
-        ["CHN", 16], ["CMR", 70], ["CHL", 15], ["CCK", 85], ["CAN", 76], ["COG", 20],
-        ["CAF", 93], ["COD", 36], ["CZE", 77], ["CYP", 65], ["CXR", 14], ["CRI", 31],
-        ["CUW", 67], ["CPV", 63], ["CUB", 40], ["SWZ", 58], ["SYR", 96], ["SXM", 31]]
+    country_data_for_map = country_codes[['Code', 'AllergyIndex']].values.tolist()
 
-    return render_template('pages/eat_map.html', country_data=country_data)
+    # country_data = [
+    #     ["BLR", 75], ["BLZ", 43], ["RUS", 50], ["RWA", 88], ["SRB", 21], ["TLS", 43],
+    #     ["REU", 21], ["TKM", 19], ["TJK", 60], ["ROU", 4], ["TKL", 44], ["GNB", 38],
+    #     ["GUM", 67], ["GTM", 2], ["SGS", 95], ["GRC", 60], ["GNQ", 57], ["GLP", 53],
+    #     ["JPN", 59], ["GUY", 24], ["GGY", 4], ["GUF", 21], ["GEO", 42], ["GRD", 65],
+    #     ["GBR", 14], ["GAB", 47], ["SLV", 15], ["GIN", 19], ["GMB", 63], ["GRL", 56],
+    #     ["ERI", 57], ["MNE", 93], ["MDA", 39], ["MDG", 71], ["MAF", 16], ["MAR", 8],
+    #     ["MCO", 25], ["UZB", 81], ["MMR", 21], ["MLI", 95], ["MAC", 33], ["MNG", 93],
+    #     ["MHL", 15], ["MKD", 52], ["MUS", 19], ["MLT", 69], ["MWI", 37], ["MDV", 44],
+    #     ["MTQ", 13], ["MNP", 21], ["MSR", 89], ["MRT", 20], ["IMN", 72], ["UGA", 59],
+    #     ["TZA", 62], ["MYS", 75], ["MEX", 80], ["ISR", 77], ["FRA", 54], ["IOT", 56],
+    #     ["SHN", 91], ["FIN", 51], ["FJI", 22], ["FLK", 4], ["FSM", 69], ["FRO", 70],
+    #     ["NIC", 66], ["NLD", 53], ["NOR", 7], ["NAM", 63], ["VUT", 15], ["NCL", 66],
+    #     ["NER", 34], ["NFK", 33], ["NGA", 45], ["AUS", 96], ["NPL", 21], ["NRU", 13],
+    #     ["NIU", 6], ["COK", 19], ["XKX", 32], ["CIV", 27], ["CHE", 65], ["COL", 64],
+    #     ["CHN", 16], ["CMR", 70], ["CHL", 15], ["CCK", 85], ["CAN", 76], ["COG", 20],
+    #     ["CAF", 93], ["COD", 36], ["CZE", 77], ["CYP", 65], ["CXR", 14], ["CRI", 31],
+    #     ["CUW", 67], ["CPV", 63], ["CUB", 40], ["SWZ", 58], ["SYR", 96], ["SXM", 31]]
+
+    return render_template('pages/eat_map.html', country_data=country_data_for_map)
 
 
 @app.route('/login')
